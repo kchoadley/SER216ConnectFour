88fbe603a5d1f5d7907cdfa1d3820070d5b78212
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-04-27 15:48:41 -0500

    Fixed the game crashing on full board.
    Also fixed winning not detected on full board.

---
+++ ConnectFourGame/xtend-gen/connect/four/gui/JnarioTestFeature.java
@@ -0,0 +1,14 @@
+package connect.four.gui;
+
+import connect.four.gui.JnarioTestFeatureNamingThePlayer;
+import org.jnario.runner.Contains;
+import org.jnario.runner.FeatureRunner;
+import org.jnario.runner.Named;
+import org.junit.runner.RunWith;
+
+@Contains(JnarioTestFeatureNamingThePlayer.class)
+@Named("JnarioTest")
+@RunWith(FeatureRunner.class)
+@SuppressWarnings("all")
+public class JnarioTestFeature {
+}
